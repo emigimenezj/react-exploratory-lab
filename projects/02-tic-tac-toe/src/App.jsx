8f80c07cc07b171f3233a84d24eb087446a32f35
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-13 14:15:25 -0300

    Fix turns visual issue in Tic-Tac-Toe project

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -78,7 +78,7 @@ function App() {
           )
         })}
       </section>
-      <section>
+      <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
