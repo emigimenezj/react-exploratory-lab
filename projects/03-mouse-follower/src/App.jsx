f14cf4279d7103dabb592b9e5ab4bb3b697fb5ab
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-14 23:36:12 -0300

    Add mouse follower main logic

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,11 +1,53 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({x: 0, y: 0});
+
+  useEffect(() => {
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log('handleMove', {clientX, clientY});
+      setPosition({ x: clientX, y: clientY });
+    }
+
+    if (enabled) addEventListener('pointermove', handleMove);
+
+    return () => removeEventListener('pointermove', handleMove);
+
+  }, [enabled]);
 
   return (
-    <h1>Hola!</h1>
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09F',
+        border: '1px solid #000',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      ></div>
+      <button onClick={() => setEnabled(prev => !prev)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
+  )
+}
+
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
