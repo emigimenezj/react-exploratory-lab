306eeef23a1bc4f180abbcc2bb665dfd46d3fd63
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-20 19:03:39 -0300

    Add control to avoid duplicate search

--- projects/05-movie-searcher/src/hooks/useMovies.js
+++ projects/05-movie-searcher/src/hooks/useMovies.js
@@ -1,14 +1,34 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import { searchMovies } from '../services/movies.js'
 
 export function useMovies({ search }) {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const previousSearch = useRef(search);
 
   const getMovies = async () => {
-    const moviesResponse = await searchMovies({ search });
-    setMovies(moviesResponse);
+
+    if (search === previousSearch.current) return;
+
+    try {
+      setLoading(true);
+      setError(null);
+
+      previousSearch.current = search;
+
+      const moviesResponse = await searchMovies({ search });
+      setMovies(moviesResponse);
+
+    } catch(error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+    
   }
   
-  return { movies, getMovies };
+  return { movies, getMovies, loading };
 }
