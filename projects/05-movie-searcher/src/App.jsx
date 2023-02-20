2e71c21828741a0af679aa28903ad2fcc9a64372
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-20 13:29:42 -0300

    Add main layout with mocked data

--- projects/05-movie-searcher/src/App.jsx
+++ projects/05-movie-searcher/src/App.jsx
@@ -1,6 +1,13 @@
+import moviesResponse from './mocks/with-results.json';
+import errorResponse from './mocks/no-results.json';
 import './App.css'
 
 function App() {
+
+  const movies = moviesResponse.Search;
+  const hasMovies = movies?.length > 0;
+
+  console.log(movies, hasMovies);
 
   return (
     <div className='page'>
@@ -12,7 +19,23 @@ function App() {
       </header>
       
       <main>
-        Acá van a estar los resultados.
+        {hasMovies
+          ? (
+            <ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={`Poster of ${movie.Title}`} />
+                  </li>
+                ))
+              }
+            </ul>
+          ) : (
+            <p>No se encontraron películas para esta búsqueda.</p>
+          )
+        }
       </main>
     </div>
   );
