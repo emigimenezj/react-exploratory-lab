14387cc39047a01c1c9b315623a6f9205c0fd79a
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-20 14:08:37 -0300

    Refactor movies layout in separated components

---
+++ projects/05-movie-searcher/src/components/Movies.jsx
@@ -0,0 +1,31 @@
+export function Movies({ movies }) {
+
+  const hasMovies = movies?.length > 0;
+
+  return (
+    hasMovies
+      ? <ListOfMovies movies={movies} />
+      : <NoMoviesResult />
+  )
+}
+
+function ListOfMovies({ movies }) {
+  return (
+    <ul>
+      {
+        movies.map(movie => (
+          <li key={movie.id}>
+            <h3>{movie.title}</h3>
+            <p>{movie.year}</p>
+            <img src={movie.poster} alt={`Poster of ${movie.title}`} />
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+function NoMoviesResult() {
+  return <p>No se encontraron películas para esta búsqueda.</p>
+}
+
