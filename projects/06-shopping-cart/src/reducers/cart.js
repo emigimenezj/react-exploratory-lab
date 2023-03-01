717888a40360d346cb6e1d25175f3eb89d138219
Emilio Giménez <emigimenezj@zohomail.com>
2023-03-01 03:24:33 -0300

    Add a reducer for cart state handler functions

---
+++ projects/06-shopping-cart/src/reducers/cart.js
@@ -0,0 +1,26 @@
+
+export const cartInitialState = [];
+
+export const cartReducer = (state, {type, payload}) => {
+
+  const HANDLERS = {
+    'ADD_TO_CART': () => {
+      const { id } = payload;
+      const productInCartIndex = state.findIndex(item => item.id === id);
+
+      if (productInCartIndex >= 0) {
+        const newState = structuredClone(state);
+        newState[productInCartIndex].quantity += 1;
+        return newState;
+      }
+
+      return [
+        ...state,
+        {...payload, quantity: 1}
+      ];
+    },
+    'REMOVE_FROM_CART': () => state.filter(item => item.id !== payload.id),
+    'CLEAR_CART': () => cartInitialState
+  }
+  return HANDLERS[type]();
+}
