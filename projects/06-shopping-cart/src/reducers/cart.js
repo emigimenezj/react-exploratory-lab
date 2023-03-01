d881f7cc3948816c47116df7d4bd00a275a6c1d3
Emilio Giménez <emigimenezj@zohomail.com>
2023-03-01 14:32:12 -0300

    Add localStorage  support for persist cart data

--- projects/06-shopping-cart/src/reducers/cart.js
+++ projects/06-shopping-cart/src/reducers/cart.js
@@ -1,8 +1,11 @@
 
-export const cartInitialState = [];
+export const cartInitialState = JSON.parse(localStorage.getItem('cart')) || [];
+
+export const updateLocalStorage = state => {
+  localStorage.setItem('cart', JSON.stringify(state));
+}
 
 export const cartReducer = (state, {type, payload}) => {
-
   const HANDLERS = {
     'ADD_TO_CART': () => {
       const { id } = payload;
@@ -22,5 +25,9 @@ export const cartReducer = (state, {type, payload}) => {
     'REMOVE_FROM_CART': () => state.filter(item => item.id !== payload.id),
     'CLEAR_CART': () => cartInitialState
   }
-  return HANDLERS[type]();
+
+  const newState = HANDLERS[type]();
+
+  updateLocalStorage(newState);
+  return newState;
 }
