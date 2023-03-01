387d0ddaf36b9d9c3a2856aee44433c1063375e9
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-28 23:31:02 -0300

    Add filter

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,7 +1,28 @@
+import { products as initialProducts } from './mocks/products.json';
+import { Products } from './components/Products';
+import { useState } from 'react';
+
 function App() {
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  });
+
+  const filterProducts = products => {
+    return products.filter(({price, minPrice, category}) => {
+      return (
+        price >= filters.minPrice &&
+        (filters.category === 'all' ||
+        filters.category === category)
+      );
+    });
+  }
+
+  const filteredProducts = filterProducts(products);
 
   return (
-    <h1>Shopping cart!</h1>
+    <Products products={filteredProducts} />
   );
 }
 
