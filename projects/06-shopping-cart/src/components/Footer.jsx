01a4a8144ccabbea41874af5b11e968a85e3ce4e
Emilio Giménez <emigimenezj@zohomail.com>
2023-03-01 02:27:13 -0300

    Add cart info in footer debugger

--- projects/06-shopping-cart/src/components/Footer.jsx
+++ projects/06-shopping-cart/src/components/Footer.jsx
@@ -1,13 +1,17 @@
-import { useContext } from 'react';
-import { FiltersContext } from '../context/filters';
+import { useCart } from '../hooks/useCart';
+import { useFilters } from '../hooks/useFilters';
 import './Footer.css'
 
 export function Footer() {
-  const { filters } = useContext(FiltersContext);
+  const { filters } = useFilters();
+  const { cart } = useCart();
+
+  const cartFormatted = cart.map(({title, quantity}) => ({ title, quantity }));
 
   return (
     <footer className="footer">
-      {JSON.stringify(filters, null, 2)}
+      {JSON.stringify(filters, null, 2)} <br/>
+      {JSON.stringify(cartFormatted, null, 2)}
     </footer>
   );
 }
