ebe8ed6a949e7cee3839e60482831939cd1c1809
Emilio Giménez <emigimenezj@zohomail.com>
2023-03-24 23:40:35 -0300

    Add route support to use with the Router

--- projects/07-router/src/App.jsx
+++ projects/07-router/src/App.jsx
@@ -2,20 +2,26 @@ import Router from './Router';
 import AboutPage from './pages/About';
 import HomePage from './pages/Home';
 import SearchPage from './pages/Search';
+import { Route } from './Route';
+
+const appRoutes = [
+  {
+    path: '/search/:query',
+    Component: SearchPage
+  },
+  {
+    path: '/emi',
+    Component: () => <h1>You was totally PWNED by Emi! :v</h1>
+  },
+];
 
 function App() {
   return (
     <main>
-      <Router routes={[
-        {
-    path: '/search/:query',
-    Component: SearchPage
-        },
-        {
-          path: '/emi',
-          Component: () => <h1>You was totally PWNED by Emi! :v</h1>
-        },
-      ]} />
+      <Router routes={appRoutes}>
+        <Route path='/' Component={HomePage} />
+        <Route path='/about' Component={AboutPage} />
+      </Router>
     </main>
   );
 }
--- projects/07-router/src/Router.jsx
+++ projects/07-router/src/Router.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import { match } from 'path-to-regexp';
+import { Children, useEffect, useState } from 'react';
 import { EVENTS } from './const';
 
-export default function Router({ routes = [], defaultComponent: DefaultComponent = () => <h1>404: Not Found!</h1> }) {
+export default function Router({ children, routes = [], defaultComponent: DefaultComponent = () => <h1>404: Not Found!</h1> }) {
 
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
@@ -20,7 +21,28 @@ export default function Router({ routes = [], defaultComponent: DefaultComponent
 
   }, []);
 
-  const Page = routes.find(({ path }) => path === currentPath)?.Component;
+  let routeParams = {};
 
-  return Page ? <Page /> : <DefaultComponent />;
+  const routesFromChildren = Children
+    .map(children, ({ props, type }) => type.name === 'Route' ? props : null)
+    .filter(Boolean);
+
+  const routesToUse = routes.concat(routesFromChildren);
+
+  const Page = routesToUse.find(({ path }) => {
+    if (path === currentPath) return true;
+
+    const matcherUrl = match(path, { decode: decodeURIComponent });
+    const matched = matcherUrl(currentPath);
+    
+
+    if(!matched) return false;
+
+    routeParams = matched.params
+    return true;
+  })?.Component;
+
+  return Page
+    ? <Page routeParams={routeParams} />
+    : <DefaultComponent routeParams={routeParams} />;
 }
