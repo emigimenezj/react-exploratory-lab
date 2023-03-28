6a6ea3424fa30e775f09578b33eae65f5faef941
Emilio Giménez <emigimenezj@zohomail.com>
2023-03-28 02:30:17 -0300

    Add feature to complete todos

--- projects/08-todo-app-ts/src/components/Todo.tsx
+++ projects/08-todo-app-ts/src/components/Todo.tsx
@@ -1,23 +1,24 @@
-import { type Todo as TodoType } from '../types';
+import { TodoId, type Todo as TodoType } from '../types';
 
 interface Props extends TodoType {
-  onRemoveTodo: (id: string) => void
+  onToggleCompleteTodo: ({ id, completed } : Pick<TodoType, 'id' | 'completed'>) => void
+  onRemoveTodo: ({ id }: TodoId) => void
 }
 
-export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo }) => {
+export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo, onToggleCompleteTodo }) => {
   return (
     <div className="view">
       <input
         className="toggle"
         checked={completed}
         type="checkbox"
-        onChange={() => {}}
+        onChange={(event) => { onToggleCompleteTodo({ id, completed: event.target.checked }) }}
       />
       <label>{title}</label>
       <button
         className="destroy"
         onClick={() => {
-          onRemoveTodo(id);
+          onRemoveTodo({ id });
         }}
       />
     </div>
