7ddf63290ae0eb59c16034b5cc995b4f24ac5fb0
Emilio Giménez <emigimenezj@zohomail.com>
2023-03-28 00:58:23 -0300

    Add Todo List, Todos and remove Todos function

--- projects/08-todo-app-ts/src/App.tsx
+++ projects/08-todo-app-ts/src/App.tsx
@@ -1,10 +1,40 @@
 import { useState } from 'react';
+import { Todos } from './components/Todos';
 
-function App() {
-  const [count, setCount] = useState(0);
+const mockTodos = [
+  {
+    id: '1',
+    title: 'Aprender TS',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'Aprender Rust',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'Aprender Blockchain',
+    completed: false
+  }
+];
+
+function App(): JSX.Element {
+
+  const [todos, setTodos] = useState(mockTodos);
+
+  const handleRemove = (id: string): void => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
+  }
 
   return (
-    <h1>HOLA</h1>
+    <div className="todoapp">
+      <Todos
+        onRemoveTodo = {handleRemove}
+        todos={todos}
+      />
+    </div>
   );
 }
 
