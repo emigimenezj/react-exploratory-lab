d4d8c4496d2fab3dcbb965c259c9aef47281c836
Emilio Giménez <emigimenezj@zohomail.com>
2023-05-10 01:20:04 -0300

    Add home formatting with react bootstrap

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,72 +1,39 @@
-import { useReducer } from 'react';
-
-import { Action, type State } from './types.d';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, Row, Col } from 'react-bootstrap';
+
 import './App.css';
-
-const initialState: State = {
-  fromLanguage: 'auto',
-  toLanguage: 'en',
-  fromText: '',
-  result: '',
-  loading: false
-}
-
-function reducer(state: State, action: Action) {
-  const { type } = action;
-
-  if (type === 'INTERCHANGE_LANGUAGES') {
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage
-    }
-  }
-
-  if (type === 'SET_FROM_LANGUAGE') {
-    return {
-      ...state,
-      fromLanguage: action.payload
-    }
-  }
-
-  if (type === 'SET_TO_LANGUAGE') {
-    return {
-      ...state,
-      toLanguage: action.payload
-    }
-  }
-
-  if (type === 'SET_FROM_TEXT') {
-    return {
-      ...state,
-      loading: true,
-      fromText: action.payload,
-      result: ''
-    }
-  }
-
-  if (type === 'SET_RESULT') {
-    return {
-      ...state,
-      loading: false,
-      result: action.payload
-    }
-  }
-
-  return state;
-}
+import { useStore } from './hooks/useStore';
 
 function App() {
 
+  const { 
+    fromLanguage,
+    toLanguage,
+    interchangeLanguage
+  } = useStore();
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log({fromLanguage});
 
   return (
-    <div className="App">
+    <Container fluid>
       <h1>Google Translate</h1>
-    </div>
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+        <Col>
+          <button onClick={interchangeLanguage}>
+            intercabmiar
+          </button>
+        </Col>
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
