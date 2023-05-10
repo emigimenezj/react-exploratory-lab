010e022ba109fdce0a5de06b01bad9628a6788ff
Emilio Giménez <emigimenezj@zohomail.com>
2023-05-09 23:58:32 -0300

    Add basic actions to the reducer

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,7 +1,62 @@
+import { useReducer } from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+const initialState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+function reducer(state, action) {
+  const { type, payload } = action;
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      fromText: payload
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      result: payload
+    }
+  }
+
+  return state;
+}
+
 function App() {
+
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="App">
